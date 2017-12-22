96f8ca9cd6c0fa43e84d0def40aa8c06648b5946
Meiggin <781571871@qq.com>
2017-12-22 17:38:48 +0800

    no message

---
+++ 09.components/04.vue-root-component/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Server Status: {{ status }}</h1>
+</template>
+
+<script>
+// 如果将data直接编写成对象的话，系统会报错，告诉你data必须是一个function函数
+/*
+export default {
+    data:{
+        status: 'Critical'
+    }
+}
+*/
+
+
+export default {
+    data:function(){
+        return {
+            status: 'Critical'
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 09.components/05.creating-a-component/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <app-server-status></app-server-status>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
