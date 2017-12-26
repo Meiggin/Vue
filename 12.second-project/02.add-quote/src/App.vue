31df688eb5481baf66dd48d0771c21d2b1ea20bc
Meiggin <781571871@qq.com>
2017-12-26 19:25:31 +0800

    no message

---
+++ 12.second-project/02.add-quote/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+  	<app-quote-grid :quotes="quotes"></app-quote-grid>
+  </div>
+</template>
+
+<script>
+import QuoteGrid from './components/QuoteGrid.vue';
+import NewQuote from './components/NewQuote.vue';
+
+
+export default {
+  data:function(){
+    return {
+      quotes:[
+        'Just a Quote to see something'
+      ],
+      maxQuotes:10
+    }
+  },
+  methods:{
+    newQuote(quote){
+      this.quotes.push(quote);
+    }
+  },
+  components:{
+    appQuoteGrid:QuoteGrid,
+    appNewQuote:NewQuote
+  }
+}
+</script>
+
+<style>
+
+</style>
