742ff7a59bc0b927f7763012920345ce8f80538a
Meiggin <jiang781571871@gmail.com>
2018-01-07 18:46:30 +0800

    Add files via upload

---
+++ 15.filter-mixins/04.creating-and-using-mixins/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+  	<div class="row">
+  		<div class="col-xs-12 col-sm-8 col-sm-offset-2 
+      col-md-6 col-md-offset-3">
+  			<h1>本地过滤器</h1>
+  			<p>{{ text | toUppercase | to-lowercase }}</p>
+        <hr>
+        <!-- 按钮操作说明的是对象的作用域问题 -->
+        <button @click="fruits.push('Berries')">
+        Add New Item
+        </button>
+        <input v-model="filterText">
+        <ul>
+          <li v-for="fruit in filteredFruits">{{ fruit }}</li>
+        </ul>
+  	    <hr>
+        <app-list></app-list>
+    	</div>
+  	</div>
+  </div>
+</template>
+
+<script>
+import List from './List.vue';
+import {fruitMixin} from './fruitMixin';
+
+export default {
+  mixins: [fruitMixin],
+  data(){
+    return {
+      text:'Hello Vue!'
+    }
+  },
+  filters:{
+    toUppercase(value){
+      return value.toUpperCase();
+    }
+  },
+  components:{
+    appList:List
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ 15.filter-mixins/04.creating-and-using-mixins/src/List.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+			<h1>过滤器和混合</h1>
+      <input v-model="filterText">
+      <ul>
+        <li v-for="fruit in filteredFruits">
+        {{ fruit }}
+        </li>
+      </ul>
+		</div>
+</template>
+
+<script>
+import {fruitMixin} from './fruitMixin';
+
+export default {
+  mixins: [fruitMixin],
+  created(){
+    console.log('inside created hook')
+  }
+}
+</script>
+
+<style>
+
+</style>
