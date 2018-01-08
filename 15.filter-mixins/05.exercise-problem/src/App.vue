f93a623bda73e21c5495ddc783c9baeac44e5762
Meiggin <jiang781571871@gmail.com>
2018-01-08 18:09:14 +0800

    Add files via upload

---
+++ 15.filter-mixins/05.exercise-problem/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+  	<div class="row">
+  		<div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+  			<h1>过滤器和混合器</h1>
+  			<!--
+        1)建立一个本地过滤器，可以将文本内容反转显示
+        2)建立一个全局过滤器，可以统计一个字符串长度，并追加到字符串后面，如Test (4)
+        3)实现1、2功能，但需要用computed属性
+        4)通过computed属性实现2的功能，但是通过Mixin
+        -->
+    	</div>
+     
+  	</div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
